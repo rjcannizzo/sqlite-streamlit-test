eaf6889e7e09492a7cd50237c2944ca7c5002ffa
= <rc.info@protonmail.com>
2022-10-22 05:34:05 -0500

    added Database class

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
-from utils import get_data, transform
+from utils import Database
+db = Database()
+
 st.title('SQLite Test')
 
 
-raw_data = get_data()
-data = transform(raw_data)
+raw_data = db.get_data()
+data = db.transform(raw_data)
 df = pd.DataFrame(data)
 amount = st.slider("Reps", 5, 50, 10, 5)
 df[df.reps > amount]
--- utils.py
+++ utils.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import sqlite3
 
-@st.cache(suppress_st_warning=True)
-def get_data():
-    st.write('getting data from db...')
-    conn = sqlite3.connect('exercise.db')
-    cursor = conn.cursor()
-    cursor.execute('select * from exercise')
-    return cursor.fetchall()
+class Database:
 
-def transform(input):
-    data = []
-    for item in input:
-        date, exercise, reps, _ = item
-        data.append(dict(date=date, exercise=exercise, reps=reps))
-    return data
+    @st.cache(suppress_st_warning=True)
+    def get_data(self):
+        st.write('getting data from db...')
+        conn = sqlite3.connect('exercise.db')
+        cursor = conn.cursor()
+        cursor.execute('select * from exercise')
+        return cursor.fetchall()
+
+    def transform(self, input):
+        data = []
+        for item in input:
+            date, exercise, reps, _ = item
+            data.append(dict(date=date, exercise=exercise, reps=reps))
+        return data
